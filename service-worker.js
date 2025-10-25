20582ac4b0023a825f77333b2461f131b35277b3
aiko030500-wq <aiko030500@gmail.com>
2025-10-25 12:51:04 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('ai-bayan-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/module1.html',
+        '/module2.html',
+        '/module3.html',
+        '/module4.html',
+        '/chat.html',
+        '/assets/background_glow.jpg',
+        '/assets/logo_girl.png',
+        '/assets/icon512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
