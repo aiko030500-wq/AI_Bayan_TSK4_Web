192d016862293fe6936403a00479fcab8c7a2273
aiko030500-wq <aiko030500@gmail.com>
2025-10-25 13:41:46 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('ai-bayan-cache-v1').then(cache => {
+    caches.open('ai-bayan-cache-v2').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/module1.html',
-        '/module2.html',
-        '/module3.html',
-        '/module4.html',
-        '/chat.html',
-        '/assets/background_glow.jpg',
-        '/assets/logo_girl.png',
-        '/assets/icon512.png'
+        './',
+        './index.html',
+        './module1.html',
+        './module2.html',
+        './module3.html',
+        './module4.html',
+        './chat.html',
+        './manifest.json',
+        './assets/background_glow.jpg',
+        './assets/logo_girl.png',
+        './assets/icon512.png'
       ]);
     })
   );
   self.skipWaiting();
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
